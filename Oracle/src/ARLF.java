a921cf590f39634ab3d02f2986cf75b181b96d1d
manuelpc97 <manuelpc97@unitec.edu>
2016-08-26 18:26:10 -0600

    Metodo agregar de ARLF

---
+++ Oracle/src/ARLF.java
@@ -0,0 +1,46 @@
+
+import java.io.RandomAccessFile;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author manuel
+ */
+public class ARLF {
+    
+    String direccion = "";
+    
+    public ARLF(String direccion, int sizeRegistro, int sizeCampo) {
+        this.direccion = direccion + ".txt";
+        try {
+            RandomAccessFile archivo = new RandomAccessFile(this.direccion, "rw");
+            archivo.writeBytes(direccion + "°" + sizeRegistro + "|" + sizeCampo+"|");
+        } catch (Exception e) {
+            System.out.println("Error al crear el archivo");
+        }
+    }
+    
+    public void addRegistro(String header){
+        try {
+            RandomAccessFile archivo = new RandomAccessFile(direccion,"rw");
+            archivo.seek(archivo.length());
+            archivo.writeChars(header+"~");
+        } catch (Exception e) {
+            System.out.println("Error al agregar header");
+        }
+    }
+    
+    public void addCampo(String campo){
+        try {
+            RandomAccessFile archivo = new RandomAccessFile(direccion,"rw");
+            archivo.seek(archivo.length());
+            archivo.writeChars(campo);
+        } catch (Exception e) {
+            System.out.println("Error al agregar el campo.");
+        }
+    }
+}
---
+++ Oracle/src/ARLV.java
@@ -0,0 +1,135 @@
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author manuel
+ */
+public class ARLV {
+    String direccion = "";
+
+    public ARLV(String nombre) {
+        direccion = "./" + nombre+".txt";
+    }
+    
+    public void agregarRegistro(int adminRegistro, int adminCampo){
+       
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
