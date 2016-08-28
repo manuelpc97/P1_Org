77813001133f648b671bf396290d6d4f236b8364
manuelpc97 <manuelpc97@unitec.edu>
2016-08-27 20:57:44 -0600

    Metodos para concer administracion hechos

--- Oracle/src/ARLV.java
+++ Oracle/src/ARLV.java
@@ -1,7 +1,9 @@
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 
@@ -19,8 +21,13 @@ public class ARLV {
     String direccion = "";
 
     public ARLV() {
-    
+
     }
+
+    public ARLV(String dir) {
+        direccion = dir;
+    }
+
     
     public ARLV(String nombre, int adminRegistro, int adminCampo) {
         direccion = "./" + nombre + ".txt";
@@ -36,10 +43,93 @@ public class ARLV {
         try {
             RandomAccessFile archivo = new RandomAccessFile(direccion, "rw");
             archivo.seek(archivo.length());
-            archivo.writeBytes(cantidadCampos +"|^" +header + "}");
+            archivo.writeBytes(cantidadCampos + "|^" + header + "}");
         } catch (Exception e) {
             System.out.println("Error al cargar header");
         }
 
+    }
+
+    public int getTipoAdministracionRegistros() throws FileNotFoundException, IOException {
+        int retorno = 0;
+        int posicion = 0;
+        int posicion2 = 0;
+        String number = "";
+
+        RandomAccessFile archivo = new RandomAccessFile(direccion, "rw");
+
+        for (int i = 0; i < archivo.length(); i++) {
+            archivo.seek(i);
+            if (((char) archivo.readByte()) == '~') {
+                posicion = i;
+            } else if (((char) archivo.readByte()) == '|') {
+                posicion2 = i;
+                i = (int) archivo.length();
+            }
+        }
+
+        for (int i = posicion + 1; i < posicion2; i++) {
+            archivo.seek(i);
+            number += ((char) archivo.readByte());
+        }
+
+        retorno = Integer.parseInt(number);
+        return retorno;
+    }
+    
+    public int getTipoAdministracionCampos() throws FileNotFoundException, IOException{
+        int retorno = 0;
+        int contador = 0;
+        int pos1 = 0;
+        int pos2 = 0;
+        String numero = "";
+        RandomAccessFile archivo = new RandomAccessFile(direccion,"rw");
+        
+        for (int i = 0; i < archivo.length(); i++) {
+            archivo.seek(i);
+            if(((char)archivo.readByte()) == '|' && contador == 0){
+                pos1 = i;
+                contador++;
+            }else if(((char)archivo.readByte()) == '|' && contador == 1){
+                pos2 = i;
+                i = (int)archivo.length();
+            }
+        }
+        
+        for (int i = pos1+1; i < pos2; i++) {
+            archivo.seek(i);
+            numero+=((char)archivo.readByte());
+        }
+        retorno = Integer.parseInt(numero);
+        return retorno;
+    }
+    
+    public int getCantidadDeCampos() throws FileNotFoundException, IOException{
+        int retorno = 0;
+        int contador = 0;
+        int pos1 = 0;
+        int pos2 = 0;
+        String numero = "";
+        RandomAccessFile archivo = new RandomAccessFile(direccion,"rw");
+        
+        for (int i = 0; i < archivo.length(); i++) {
+            archivo.seek(i);
+            if(((char)archivo.readByte()) == '|' && contador == 0){
+                contador++;
+            }else if(((char)archivo.readByte()) == '|' && contador == 1){
+                pos1 = i;
+                contador++;
+            }else if(((char)archivo.readByte()) == '|' && contador == 2){
+                pos2 = i;
+                i = (int)archivo.length();
+            }
+        }
+        
+        for (int i = pos1+1; i < pos2; i++) {
+            archivo.seek(i);
+            numero+=((char)archivo.readByte());
+        }
+        retorno = Integer.parseInt(numero);
+        return retorno;
     }
 }
